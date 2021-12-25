2e5af241a38a299c00480630d751d64afa51e1b5
Eric Ballou <eballou@umass.edu>
2021-12-25 03:31:24 -0500

    attempt at timeline

--- js/script.js
+++ js/script.js
@@ -6,8 +6,37 @@ function onLoad() {
     let themeSwitcher = document.querySelector("#theme-switch");
     themeSwitcher.addEventListener("mouseup", themeSwitch, false);
 
-    // on scroll header
-    
+    //
+
+    let sectionArr = [
+        '#header-section', '#about-section', '#now-section',
+        '#education-section', '#projects-section', '#skills-section'
+    ];
+
+    let lateralArr = [
+        '#lateral-header', '#lateral-about', '#lateral-now',
+        '#lateral-education', '#lateral-projects', '#lateral-skills'
+    ];
+
+    sectionArr.forEach(function(section, index) {
+        let offsetStr = '50%';
+        if (index === sectionArr.length - 1) {
+            offsetStr = 'bottom-in-view';
+        }
+        $(section).waypoint(function(direction) {
+            // lateralArr.forEach(function(lateral) {
+            //     $(lateral).removeClass('active');
+            // });
+            console.log(lateralArr[index] + ' is triggered');
+            if (direction === 'up') {
+                $(lateralArr[index]).removeClass('active');
+            } else {
+                $(lateralArr[index]).addClass('active');
+            }
+        }, {
+            offset: offsetStr
+        });
+    });
 }
 
 function themeSwitch() {
@@ -19,8 +48,5 @@ function themeSwitch() {
     document.documentElement.setAttribute("data-theme", newTheme);
 }
 
-function onScrollHeader() {
-
-}
 
 onLoad();
