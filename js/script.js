2c4c7ecdf2771acd9b95af09e8069f26206d7c3b
Eric Ballou <eballou@umass.edu>
2021-12-25 05:38:59 -0500

    fixed timeline

--- js/script.js
+++ js/script.js
@@ -6,37 +6,7 @@ function onLoad() {
     let themeSwitcher = document.querySelector("#theme-switch");
     themeSwitcher.addEventListener("mouseup", themeSwitch, false);
 
-    //
-
-    let sectionArr = [
-        '#header-section', '#about-section', '#now-section',
-        '#education-section', '#projects-section', '#skills-section'
-    ];
-
-    let lateralArr = [
-        '#lateral-header', '#lateral-about', '#lateral-now',
-        '#lateral-education', '#lateral-projects', '#lateral-skills'
-    ];
-
-    sectionArr.forEach(function(section, index) {
-        let offsetStr = '50%';
-        if (index === sectionArr.length - 1) {
-            offsetStr = 'bottom-in-view';
-        }
-        $(section).waypoint(function(direction) {
-            // lateralArr.forEach(function(lateral) {
-            //     $(lateral).removeClass('active');
-            // });
-            console.log(lateralArr[index] + ' is triggered');
-            if (direction === 'up') {
-                $(lateralArr[index]).removeClass('active');
-            } else {
-                $(lateralArr[index]).addClass('active');
-            }
-        }, {
-            offset: offsetStr
-        });
-    });
+    navTimeline();
 }
 
 function themeSwitch() {
@@ -48,5 +18,37 @@ function themeSwitch() {
     document.documentElement.setAttribute("data-theme", newTheme);
 }
 
+function navTimeline() {
+    let sectionArr = [
+        '#header-section', '#about-section', '#now-section',
+        '#education-section', '#projects-section', '#skills-section'
+    ];
+
+    let lateralArr = [
+        '#lateral-header', '#lateral-about', '#lateral-now',
+        '#lateral-education', '#lateral-projects', '#lateral-skills'
+    ];
+
+    sectionArr.forEach(function(section, index) {
+        let offsetStr = '30%';
+        if (index === sectionArr.length - 1) {
+            offsetStr = 'bottom-in-view';
+        }
+        $(section).waypoint(function(direction) {
+            console.log(lateralArr[index] + ' is triggered');
+            if (direction === 'up') {
+                $(lateralArr[index - 1]).addClass('active');
+                $(lateralArr[index]).removeClass('active');
+            } else {
+                $(lateralArr[index]).addClass('active');
+                for (let i = 0; i < lateralArr.length; i++) {
+                    if (index !== i) $(lateralArr[i]).removeClass('active');
+                }
+            }
+        }, {
+            offset: offsetStr
+        });
+    });
+}
 
 onLoad();
