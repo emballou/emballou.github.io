cf256b11ba14d87be3c0be42b46df08e853741ce
Eric Ballou <eballou@umass.edu>
2021-12-26 03:17:38 -0500

    fixed timeline, updated highlight

--- js/script.js
+++ js/script.js
@@ -35,20 +35,50 @@ function navTimeline() {
             offsetStr = 'bottom-in-view';
         }
         $(section).waypoint(function(direction) {
+
             console.log(lateralArr[index] + ' is triggered');
-            if (direction === 'up') {
-                $(lateralArr[index - 1]).addClass('active');
-                $(lateralArr[index]).removeClass('active');
-            } else {
-                $(lateralArr[index]).addClass('active');
-                for (let i = 0; i < lateralArr.length; i++) {
-                    if (index !== i) $(lateralArr[i]).removeClass('active');
-                }
-            }
+
+            // if scrolling up, current index is inactive, next index (index - 1) is active
+            let directionBool = direction === 'up';
+            // create new active, remove inactive
+            window.requestAnimationFrame(function() {
+                playGrow(lateralArr[index], sectionArr[index], !directionBool);
+            });
+            
+            // remove old active, add inactive
+            window.requestAnimationFrame(function() {
+                playGrow(lateralArr[index - 1], sectionArr[index - 1], directionBool);
+            });
+
         }, {
             offset: offsetStr
         });
     });
+}
+
+function playGrow(e, s, isActive) {
+    if (e === undefined) return;
+    if (isActive && $(e).hasClass("active")) return;
+    if (!isActive && $(e).hasClass("inactive")) return;
+
+    // edit the timeline
+    $(e).removeClass("active");
+    $(e).removeClass("inactive");
+    if (isActive) {
+        $(e).addClass("active");
+    } else {
+        $(e).addClass("inactive");
+    }
+    console.log(e + " : " + $(e).attr("class").split(/\s+/));
+
+    // edit the div.page
+    $(s).removeClass("page-highlight-on");
+    $(s).removeClass("page-highlight-off");
+    if (isActive) {
+        $(s).addClass("page-highlight-on");
+    } else {
+        $(s).addClass("page-highlight-off");
+    }
 }
 
 onLoad();
